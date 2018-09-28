ad2a83a9feca08d166b567c41557c0d3c71d2bef
Lars van de Vrede <Lars van de Vrede>
2018-09-28 11:47:08 +0200

    Plz just work

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
+
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
 # Change this the name of your project. This will be the name of the final executables as well.
-project="NorthSeaPort"
+project="ci-build"
+
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -11,6 +14,7 @@ echo "Attempting to build $project for Windows"
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
+
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -20,6 +24,7 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
+
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -29,5 +34,6 @@ echo "Attempting to build $project for Linux"
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
+
 echo 'Logs from build'
-cat $(pwd)/unity.log 
+cat $(pwd)/unity.log
